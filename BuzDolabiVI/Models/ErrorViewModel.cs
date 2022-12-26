0d02ff5c3ce8a49f08acb43442ed6ea0d6bd344c
Sezer Berber <nyperu14@gmail.com>
2022-12-26 22:55:20 +0300

    a

--- BuzDolabiVI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BuzDolabiVI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
